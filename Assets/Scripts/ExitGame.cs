f881003e7ff413de4038941d85bf13c97004d38e
JackieChen <2864491906@qq.com>
2022-12-19 17:22:31 +0800

    添加退出功能

--- Assets/Scripts/ExitGame.cs
+++ Assets/Scripts/ExitGame.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class ExitGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void QuitGame()
     {
-        
-    }
+#if UNITY_EDITOR //如果是在编辑器环境下
+        UnityEditor.EditorApplication.isPlaying = false;
+#else//在打包出来的环境下
+    Application.Quit();
+#endif
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
