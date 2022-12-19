19d2ea519ee6be2a83f98edc7849b8959e5b371f
JackieChen <2864491906@qq.com>
2022-12-19 17:28:04 +0800

    WIP

--- Assets/Scripts/ExitGame.cs
+++ Assets/Scripts/ExitGame.cs
@@ -6,9 +6,9 @@ public class ExitGame : MonoBehaviour
 {
     public void QuitGame()
     {
-#if UNITY_EDITOR //如果是在编辑器环境下
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-#else//在打包出来的环境下
+#else
     Application.Quit();
 #endif
 
