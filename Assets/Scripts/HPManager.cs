b75910b2b6771ac6cd80b5a935d0c9f58b5719f1
JackieChen <2864491906@qq.com>
2022-12-19 16:33:05 +0800

    添加开始画面

---
+++ Assets/Scripts/HPManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPManager : MonoBehaviour
+{
+    public GameObject heart;
+    public GameObject halfHeart;
+    public GameObject emptyHeart;
+    private float HP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP = PlayerPrefs.GetFloat("playerHP");
+        int heartNumber = (int)HP / 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HP = PlayerPrefs.GetFloat("playerHP");
+
+    }
+}
