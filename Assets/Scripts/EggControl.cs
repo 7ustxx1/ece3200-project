953f11f1df0c5c9714914f4448a00c6ed921ee6b
JackieChen <2864491906@qq.com>
2022-12-20 17:36:19 +0800

    WIP

--- Assets/Scripts/EggControl.cs
+++ Assets/Scripts/EggControl.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class EggControl : MonoBehaviour
 {
+    private float HP = 100;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Die();
+    }
+
+    void Die()
+    {
+        animator.SetBool("isDead", true);
+    }
+
+    public void Remove()
+    {
+        Destroy(gameObject);
     }
 }
