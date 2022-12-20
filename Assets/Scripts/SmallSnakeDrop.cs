a52f0a2632f8203f2d6ccffdcfe0610072db368a
JackieChen <2864491906@qq.com>
2022-12-20 22:28:03 +0800

    update

--- Assets/Scripts/SmallSnakeDrop.cs
+++ Assets/Scripts/SmallSnakeDrop.cs
@@ -7,19 +7,25 @@ public class SmallSnakeDrop : MonoBehaviour
 {
 
     public GameObject snakePre;
-    private bool snakeEnable = false;
+    private int snakeEnable = 0;
 
     void Start()
     {
         StartCoroutine("Drop");
         PlayerPrefs.SetFloat("snakeDropSpeed", 5);
+        PlayerPrefs.SetInt("SnakeEnable", 0);
+    }
+
+    private void Update()
+    {
+        snakeEnable = PlayerPrefs.GetInt("SnakeEnable");
     }
 
     IEnumerator Drop()
     {
         while (true)
         {
-            if (snakeEnable)
+            if (snakeEnable != 0)
             {
                 GameObject.Instantiate(snakePre, transform.GetChild(Random.Range(1, 8)));
 
