64aa66932f6f9137b4ca71f55e9a3765b4662c33
JackieChen <2864491906@qq.com>
2022-12-20 15:19:52 +0800

    添加近战

---
+++ Assets/Scripts/TailPartControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TailPartControl : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Bolt")
+        {
+            Debug.Log("hit");
+            gameObject.GetComponentInParent<TailControl>().takeDamage(10);
+        }
+    }
+}
